0e38d5ff182af59ec292ab64af1421562aa61aec
nceccanti <nicceccanti@gmail.com>
2022-03-22 14:43:34 -0600

    sleep

--- listener.py
+++ listener.py
@@ -30,41 +30,49 @@ class VoiceControl:
             for i in range(n):
                 print("command left")
                 robot.pivotLeft()
+                time.sleep(0.3)
         elif s.find("turn right") != -1:
             n = s.count("right")
             for i in range(n):
                 print("command right")
                 robot.pivotRight()
+                time.sleep(0.3)
         elif s.find("neck left") != -1:
             n = s.count("left")
             for i in range(n):
                 print("command neck left")
                 robot.neckLeft()
+                time.sleep(0.3)
         elif s.find("neck right") != -1:
             n = s.count("right")
             for i in range(n):
                 print("command neck right")
                 robot.neckRight()
+                time.sleep(0.3)
         elif s.find("neck up") != -1:
             n = s.count("up")
             for i in range(n):
                 print("command neckup")
                 robot.neckUp()
+                time.sleep(0.3)
         elif s.find("neck down") != -1:
             n = s.count("down")
             for i in range(n):
                 print("command neckdown")
                 robot.neckDown()
+                time.sleep(0.3)
         elif s.find("waist left") != -1:
             n = s.count("left")
             for i in range(n):
                 print("command waist left")
                 robot.waistLeft()
+                time.sleep(0.3)
         elif s.find("waist right") != -1:
             n = s.count("right")
             for i in range(n):
                 print("command stop")
                 robot.waistRight()
+                time.sleep(0.3)
 
 
 
