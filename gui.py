e61a514274e0c558a58a80f8cbfb9d862d430dc5
nceccanti <nicceccanti@gmail.com>
2022-04-13 19:39:41 -0600

    Created basic format for drag and drop instructions.  Click black square to execute instructions.  Will change print statements based on what color is in each square

--- gui.py
+++ gui.py
@@ -7,24 +7,30 @@ class MouseMovement():
         self.myCan = c
         self.draggables = []
         self.static = []
+        self.background = []
         self.track = None
         self.trackId = None
 
     #Adds draggable object to be tracked
-    def addDraggable(self, x, y, width, height, color, id):
-        self.draggables.append([x, y, width, height, color, id])
+    def addDraggable(self, x, y, width, height, color, attribute):
+        self.draggables.append([x, y, width, height, color, attribute])
 
     #Adds static interactable object to be tracked
     def addStatic(self, x, y, width, height, color):
-        self.static.append([x, y, width, height, color, None])
+        self.static.append([x, y, width, height, color, None, None])
+
+    #Adds static background object, these objects are intended to have no interaction with the user
+    def addBackgroundObject(self, x, y, width, height, color):
+        self.background.append([x, y, width, height, color])
 
     #What happens when mouse is pressed down
     def mousePressed(self, event):
+        if event.x > 500 and event.x < 550 and event.y > 500 and event.y < 550:
+            self.execute()
         for i in range(len(self.draggables)):
             if event.x > self.draggables[i][0] and event.x < self.draggables[i][0] + self.draggables[i][2] and event.y > self.draggables[i][1] and event.y < self.draggables[i][1] + self.draggables[i][3]:
                 self.flag = True
                 self.track = i
-                self.trackId = self.draggables[i][5]
 
     #What happens when you drag the mouse
     def mouseDragged(self, event):
@@ -39,6 +45,8 @@ class MouseMovement():
 
     #Re-prints all static objects
     def printElse(self):
+        for i in self.background:
+            self.myCan.create_rectangle(i[0], i[1], i[2], i[3], fill=i[4])
         for i in self.static:
             print(i[0], i[1], i[2], i[3], i[4])
             if i[5] is None:
@@ -59,8 +67,7 @@ class MouseMovement():
             self.myCan.delete('all')
             for i in range(len(self.static)):
                 if event.x > self.static[i][0] and event.x < self.static[i][2] and event.y > self.static[i][1] and event.y < self.static[i][3]:
-                    self.static[i] = [self.static[i][0], self.static[i][1], self.static[i][2], self.static[i][3], self.static[i][4], self.draggables[self.track][4]]
-                    #Add job stack here
+                    self.static[i] = [self.static[i][0], self.static[i][1], self.static[i][2], self.static[i][3], self.static[i][4], self.draggables[self.track][4], self.draggables[self.track][5]]
             self.printElse()
             self.printDraggables()
             self.track=None
@@ -70,13 +77,19 @@ class MouseMovement():
     def rightClick(self, event):
         for i in range(len(self.static)):
             if event.x > self.static[i][0] and event.x < self.static[i][2] and event.y > self.static[i][1] and event.y < self.static[i][3]:
-                self.static[i] = [self.static[i][0], self.static[i][1], self.static[i][2], self.static[i][3], self.static[i][4], None]
+                self.static[i] = [self.static[i][0], self.static[i][1], self.static[i][2], self.static[i][3], self.static[i][4], None, None]
         self.printElse()
+
+    def execute(self):
+        print("execute instructions")
+        for i in self.static:
+            if i[6] is not None:
+                print(i[6])
 
 class GUI:
     def __init__(self, win):
         self.win = win
-        self.win.geometry("800x480")
+        self.win.geometry("1024x600")
 
     #Creates GUI window, create all objects here
     def createWindow(self):
@@ -86,17 +99,23 @@ class GUI:
         self.myCan.bind('<ButtonPress-1>', m1.mousePressed)
         self.myCan.bind('<ButtonRelease-1>', m1.mouseRelease)
         self.myCan.bind('<ButtonPress-3>', m1.rightClick)
-        self.addMoveable(5, 5, 40, 40, "#FFFF00", m1)
-        self.addMoveable(200, 5, 40, 40, "#FF0000", m1)
-        self.addDestination(400, 200, 100, 100, "#000000", m1)
+        self.myCan.bind('<space>', m1.execute())
+        self.addBackground(0, 140, 8, 1024, '#222222', m1)
+        self.addBackground(500, 500, 50, 50, '#000000', m1)
+        self.addMoveable(100, 500, 40, 40, "#FFFF00", m1, 'a')
+        self.addMoveable(200, 500, 40, 40, "#FF0000", m1, 'b')
+        self.addMoveable(300, 500, 40, 40, "#008000", m1, 'c')
+        for i in range(8):
+            x = i*128
+            self.addDestination(x+20, 100, 88, 88, "#B4E4F5", m1)
         self.myCan.pack()
 
     #Adds draggable object
-    def addMoveable(self, x, y, height, width, color, controller):
+    def addMoveable(self, x, y, height, width, color, controller, attribute):
         xf = x + width
         yf = y + height
-        temp = self.myCan.create_rectangle(x, y, xf, yf, fill=color)
-        controller.addDraggable(x, y, width, height, color, temp)
+        self.myCan.create_rectangle(x, y, xf, yf, fill=color)
+        controller.addDraggable(x, y, width, height, color, attribute)
 
     #Adds box for draggable object to be "dropped" into
     def addDestination(self, x, y, height, width, color, controller):
@@ -104,6 +123,12 @@ class GUI:
         yf = y + height
         self.myCan.create_rectangle(x, y, xf, yf, fill=color)
         controller.addStatic(x, y, xf, yf, color)
+
+    def addBackground(self, x, y, height, width, color, controller):
+        xf = x + width
+        yf = y + height
+        self.myCan.create_rectangle(x, y, xf, yf, fill=color)
+        controller.addBackgroundObject(x, y, xf, yf, color)
 
 if __name__ == '__main__':
     win = tk.Tk()
