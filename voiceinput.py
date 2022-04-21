b381d6865f8ece4a28aee650b12ccae4b039757e
nceccanti <nicceccanti@gmail.com>
2022-04-21 12:43:56 -0600

    Added voice input

---
+++ voiceinput.py
@@ -0,0 +1,19 @@
+import speech_recognition as sr
+
+class VoiceInput:
+    def listen(self, input):
+        isWord = False
+        with sr.Microphone() as source:
+            r = sr.Recognizer()
+            r.adjust_for_ambient_noise(source)
+            r.dynamic_energythreshold = 3000
+            try:
+                print("listening")
+                audio = r.listen(source)
+                print("got audio")
+                word = r.recognize_google(audio)
+                if word == input:
+                    isWord = True
+            except sr.UnknownValueError:
+                print("unknown words")
+        return isWord
