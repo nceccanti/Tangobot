2c0d5c03315801f296f93c5080d3f93c7d3da512
nceccanti <nicceccanti@gmail.com>
2022-04-22 13:21:14 -0600

    debug

--- gui.py
+++ gui.py
@@ -154,7 +154,7 @@ class MouseMovement():
                     if 0x02 == i[6][0]:
                         robot.setTarget(0x02, 6200)
                     print(self.point[self.point.index(i)][1])
-                    robot.setTarget(self.point[self.point.index(i)][1], int(float(i[6][1])))
+                    robot.setTarget(self.point[self.static.index(i)][1], int(float(i[6][1])))
                     wait = self.point[self.point.index(i)][1]
                 win2 = self.myCan
                 e = Eyes(win2)
