a1a0986b2bef9c9d88531d85faaafd5f1c4538ab
nceccanti <nicceccanti@gmail.com>
2022-05-05 13:20:39 -0600

    coffee shop removed from mainloop

--- player.py
+++ player.py
@@ -69,14 +69,14 @@ class Player:
             time.sleep(1)
         elif dir == 'B':
             self.move.setTarget(0x02, 7000)
-            time.sleep(2.5)
+            time.sleep(2.8)
             self.move.setTarget(0x02, 6000)
             self.move.setTarget(0x01, 6000)
             self.move.setTarget(0x01, 5000)
             time.sleep(1)
             self.move.setTarget(0x01, 6000)
         elif dir == 'L':
-            self.move.setTarget(0x02, 7000)
+            self.move.setTarget(0x02, 7500)
             time.sleep(1)
             self.move.setTarget(0x02, 6000)
             self.move.setTarget(0x01, 5000)
