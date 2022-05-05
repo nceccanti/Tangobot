3621eb7e8fad236fb3abdd85a03d6e32ece6f5ba
nceccanti <nicceccanti@gmail.com>
2022-05-05 12:15:45 -0600

    Arm animation attempt #3

--- Animation.py
+++ Animation.py
@@ -35,9 +35,9 @@ class Animation:
         time.sleep(0.2)
         iter = int(wait / 2)
         for i in range(iter):
-            self.move.setTarget(0x0b, 9000)
+            self.move.setTarget(0x0b, 3000)
             time.sleep(1)
-            self.move.setTarget(0x0b, 7000)
+            self.move.setTarget(0x0b, 6000)
             time.sleep(1)
         self.move.setTarget(0x05, 4000)
 
